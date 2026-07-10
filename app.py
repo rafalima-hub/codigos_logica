3ff2673aebec0fba7a975943da8b4b8f82178bf5
Rafaela Lima <rafasouzalima.rsl@gmail.com>
2026-07-10 19:45:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@
 name = "Joca da Silva"
 age = 35 
 email = "joca@mail.com"
+birthday = "10/04/1980"
+mother_fname = "Maria"
 
 print(name)
 print(age)
 print(email)
+print(birthday)
+print(mother_fname)
