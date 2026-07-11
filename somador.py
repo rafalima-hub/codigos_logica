9543d9928dda0df0db78e80d54f8310a035be6a0
Rafaela Lima <rafasouzalima.rsl@gmail.com>
2026-07-10 21:47:53 -0300

    Create somador.py

---
+++ somador.py
@@ -0,0 +1,11 @@
+# Recebe um número do usuário e soma com outro
+
+outro_numero = 5
+
+num_usuario = input("Digite um número inteiro: ")
+
+num_usuario = int(num_usuario)
+
+print(type(num_usuario))
+
+print(outro_numero + num_usuario)
