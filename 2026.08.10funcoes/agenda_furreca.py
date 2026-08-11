44573abbf5d1c87166d087a1e45637333d6bd259
Rafaela Lima <rafasouzalima.rsl@gmail.com>
2026-08-10 21:48:46 -0300

    .

--- 2026.08.10funcoes/agenda_furreca.py
+++ 2026.08.10funcoes/agenda_furreca.py
@@ -35,18 +35,3 @@ Opções
 0 - Sair do programa
 ''')
 
-opcao = input("Escolha uma opção: ")
-
-match opcao:
-    case "1":
-        pass
-    case "2":
-        pass
-    case "3":
-        pass
-    case "4":
-        pass
-    case "0":
-        pass
-    case _:
-        
